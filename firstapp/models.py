33f0a6d57ef495b1337f73824933f5e6f6bfe0c0
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-08-03 20:03:32 +0330

    add foreignkey in product model

--- firstapp/models.py
+++ firstapp/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     description = models.TextField()
     inventory = models.PositiveIntegerField()
     slug = models.SlugField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.title
