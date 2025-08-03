03e602600a1ec80e67a78fe23126378075bf8907
Alireza <ali.r.matourianpoor@gmail.com.com>
2025-08-03 19:55:46 +0330

    coding category model

--- firstapp/models.py
+++ firstapp/models.py
@@ -4,6 +4,14 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+
 class Product(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
